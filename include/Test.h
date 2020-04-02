575e856142c64f73b16e9f7e75b3d43719978c7f
lukiwa <lukasz.iwan.99@gmail.com>
2020-04-02 16:51:58 +0200

    Extended project structure. Added test class

---
+++ include/Test.h
@@ -0,0 +1,14 @@
+//
+// Created by lukasz-lap on 02.04.2020.
+//
+
+#ifndef STUD_PAMSI_GRAPHS_TEST_H
+#define STUD_PAMSI_GRAPHS_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //STUD_PAMSI_GRAPHS_TEST_H
---
+++ src/Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lukasz-lap on 02.04.2020.
+//
+
+#include "../include/Test.h"

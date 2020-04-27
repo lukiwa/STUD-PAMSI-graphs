742949fc4852a50c79b48b6d11052fddadd93b2a
lukiwa <lukasz.iwan.99@gmail.com>
2020-04-27 22:31:01 +0200

    allow cmake

---
+++ cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/AdjListGraph.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/AdjListGraph.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/AdjMatrixGraph.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/AdjMatrixGraph.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/Graph.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/Graph.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/GraphBuilder.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphBuilder.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/GraphEdge.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphEdge.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/GraphFileManager.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphFileManager.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/GraphVertex.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphVertex.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/RandomGraphGenerator.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/RandomGraphGenerator.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/ResultToFileSaver.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/ResultToFileSaver.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/UserInterface.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/UserInterface.cpp.o"
+  "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/src/main.cpp" "/home/lukasz-lap/Pulpit/Programowanie/Repozytoria_GIT/STUD_PAMSI_graphs/cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/STUD_PAMSI_graphs.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/AdjListGraph.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/AdjMatrixGraph.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/Graph.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphBuilder.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphEdge.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphFileManager.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/GraphVertex.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/RandomGraphGenerator.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/ResultToFileSaver.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/UserInterface.cpp.o"
+  "CMakeFiles/STUD_PAMSI_graphs.dir/src/main.cpp.o"
+  "STUD_PAMSI_graphs"
+  "STUD_PAMSI_graphs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STUD_PAMSI_graphs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

ecc5dc18a0e75878a239118ebed1727c2c10eec8
lukiwa <lukasz.iwan.99@gmail.com>
2020-04-27 22:24:19 +0200

    Automatic tests and directories

---
+++ cmake-build-debug/500test.sh
@@ -0,0 +1,14 @@
+
+
+#LIST
+./STUD_PAMSI_graphs -t LIST -s 500 -d 0.25 -i 100 
+./STUD_PAMSI_graphs -t LIST -s 500 -d 0.5 -i 100
+./STUD_PAMSI_graphs -t LIST -s 500 -d 0.75 -i 100 
+./STUD_PAMSI_graphs -t LIST -s 500 -d 1 -i 100 
+
+
+#MATRIX
+./STUD_PAMSI_graphs -t MATRIX -s 500 -d 0.25 -i 100 
+./STUD_PAMSI_graphs -t MATRIX -s 500 -d 0.5 -i 100 
+./STUD_PAMSI_graphs -t MATRIX -s 500 -d 0.75 -i 100 
+./STUD_PAMSI_graphs -t MATRIX -s 500 -d 1 -i 100 
